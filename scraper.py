f8e3581338fa7647941a13542407e841523d0b44
ali-hesham <ali.hesham@outlook.com>
2021-07-20 06:56:27 +0200

    save fetched data to pandas data frame , save data to csv

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 print("Enter search query...")
 
 query = input('>')
+jobs_list = []
 for i in range(50):
     url = f'https://wuzzuf.net/search/jobs/?a=navbl&q={query}&start={i}'
 
@@ -22,6 +23,12 @@ for i in range(50):
             years_experience = meta.find('div', attrs={'class': None}).span.text.replace('·', '').strip()
             skills = [skill.text.replace('·', '').strip() for skill in meta.find_all("a", class_="css-5x9pm1")]
 
+            jobs_list.append({
+                "job_title": job_title,
+                "company_name": company_name,
+                "years_experience": years_experience,
+                "skills": skills
+            })
             print(job_title)
             print(company_name)
             print(years_experience)
@@ -29,3 +36,5 @@ for i in range(50):
             print()
         except Exception as e:
             continue
+jobs_df = pd.DataFrame(jobs_list)
+jobs_df.to_csv('test.csv', index=False)
